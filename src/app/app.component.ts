48425912fae1f3c87181ce126977ee4ec89303dc
Renaud Chardin <chardin.r@sfeir.com>
2018-10-01 12:49:47 +0200

    swupdate & add button on connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {SwUpdate} from '@angular/service-worker';
 import {MatDialog} from '@angular/material';
 import {AddCommentDialogComponent} from './components/add-comment-dialog/add-comment-dialog.component';
+import {Router} from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private _fbDataBase: AngularFirestore,
-              private swUpdate: SwUpdate,
-              private dialog: MatDialog) {
+
+  private _connected = false;
+
+  constructor(
+    private _fbDataBase: AngularFirestore,
+    private swUpdate: SwUpdate,
+    private _router: Router,
+    private dialog: MatDialog) {
     this._fbDataBase.firestore.enablePersistence().then();
     this._fbDataBase.firestore.settings({
       timestampsInSnapshots: true
+    });
+    this._router.events.subscribe(() => {
+      this._connected = !!firebase.auth().currentUser;
     });
   }
 
@@ -27,6 +37,23 @@ export class AppComponent {
       width: '90%',
     });
 
+  }
+
+  public get connected() {
+    return this._connected;
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.available.subscribe(() => {
+
+        if (confirm('New version available. Load New Version?')) {
+
+          window.location.reload();
+        }
+      });
+    }
   }
 
 }
